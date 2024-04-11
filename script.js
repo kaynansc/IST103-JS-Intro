61766ba200ab7c0e82c072aede41976972533644
kaynan <kaynan.camargo@terceiro-sky.com.br>
2024-04-10 19:42:34 -0700

    add my object

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const personColorEyes = 'brown';
 console.log("Name: " + personName + ", Age: " + personAge + ", Has Driver's License: " + hasDriverLicense + ", Nickname: " + personNickname + ", Spouse: " + personSpouse + ", Color eyes: " + personColorEyes);
 
 const todoList = ['Read a book', 'Go to the gym', 'Wash the dishes', 'Clean the house', 'Make a cake']
+
+const myObject = {
+  name: 'Table',
+  color: 'Black',
+  style: 'Rounded',
+  height: '80cm',
+  width: '1,20m'
+}
