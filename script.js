3d0d73119e0446b9902481e4ef7f313acc0776c2
kaynan <kaynan.camargo@terceiro-sky.com.br>
2024-04-10 19:36:58 -0700

    add variables in javascript

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello World!");
+//alert("Hello World!");
+
+let personName = "John Doe";
+let personAge = 30;
+let hasDriverLicense = true;
+let personSalary;
+let personNickname = undefined;
+let personSpouse = null;
+const personColorEyes = 'brown';
+
