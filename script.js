95177c6bb4f954d97e1ca675f7ca5633bb718626
kaynan <kaynan.camargo@terceiro-sky.com.br>
2024-04-10 19:38:07 -0700

    adding console log

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 let personName = "John Doe";
 let personAge = 30;
 let hasDriverLicense = true;
-let personSalary;
 let personNickname = undefined;
 let personSpouse = null;
 const personColorEyes = 'brown';
 
+console.log("Name: " + personName + ", Age: " + personAge + ", Has Driver's License: " + hasDriverLicense + ", Nickname: " + personNickname + ", Spouse: " + personSpouse + ", Color eyes: " + personColorEyes);
